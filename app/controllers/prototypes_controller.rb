3fe9d9dfc2711d810ab3d78d3a5f8b318129028d
HasuikeTadakatsu <77036589+HasuikeTadakatsu@users.noreply.github.com>
2021-02-08 17:18:20 +0900

    @prototype = Prototype.find(params[:id])を追記(修正)

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -26,6 +26,7 @@ class PrototypesController < ApplicationController
   end
 
   def update
+    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
