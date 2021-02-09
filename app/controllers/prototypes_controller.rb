6f2bfe603253d72512b95ec0e35031891fb71376
HasuikeTadakatsu <77036589+HasuikeTadakatsu@users.noreply.github.com>
2021-02-09 12:38:24 +0900

    prototypesコントローラーにおいて、投稿者以外がeditアクションにアクセスしたらトップページにリダイレクトするように記述した

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :Posted_by_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -52,4 +53,8 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def Posted_by_confirmation
+    @prototype = Prototype.find(params[:id])
+    redirect_to root_path unless current_user == @prototype.user
+  end
 end
